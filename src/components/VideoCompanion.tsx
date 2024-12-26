93d319f127d0fa8ba9c1ea39179ebee28532176d
dimple amitha garuadapuri <dimpleag04@gmail.com>
2024-12-26 00:07:27 -0800

    pusher instead of web socket everything but video companion component...

--- src/components/VideoCompanion.tsx
+++ src/components/VideoCompanion.tsx
@@ -6,6 +6,7 @@ import { Button } from './ui/button';
 import { Play, Pause } from 'lucide-react';
 import { io } from 'socket.io-client';
 
+
 // const socket = io('http://localhost:3000');
 // const socket = io({
 //   path: '/api/socketio'
