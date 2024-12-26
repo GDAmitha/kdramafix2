11c341e92fe77906c632a7feded2b0a034b48a43
dimple amitha garuadapuri <dimpleag04@gmail.com>
2024-12-26 01:01:56 -0800

    tryingout prod

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,38 @@
-// import type { NextConfig } from "next";
+// // import type { NextConfig } from "next";
+
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// // };
+
+// // export default nextConfig;
+
+// import type { NextConfig } from 'next'
 
 // const nextConfig: NextConfig = {
-//   /* config options here */
-// };
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   }
+// }
 
-// export default nextConfig;
+// export default nextConfig
 
-import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  output: 'standalone',
+  poweredByHeader: false,
+  experimental: {
+    appDir: true,
   }
 }
 
-export default nextConfig
+module.exports = nextConfig
